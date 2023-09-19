f907bf8309b77c3a26296919dc3eda6cae9befae
STaliani <taliani.1841465@studenti.uniroma1.it>
2023-09-19 21:24:05 +0200

    finishes

--- run/run.py
+++ run/run.py
@@ -24,24 +24,35 @@ for key in dataset.keys():
     plt.plot(dataset[key][0,:], 'b') 
     plt.plot(dataset[key][1,:], 'orange')
      
-    # plot the noisy data
-    #plt.plot(dataset[key][2,:], 'ob')
-    #plt.plot(dataset[key][3,:], 'o', color='orange')
+    # plot the noisy 
+    #plt.figure("Torques "+  key + " sample 1")
+    plt.plot(dataset[key][2,:], 'ob')
+    plt.plot(dataset[key][3,:], 'o', color='orange')
     
-    #plt.plot(dataset[key][4,:], 'ob')
-    #plt.plot(dataset[key][5,:], 'o', color='orange')
+    #plt.figure("Torques "+  key + " sample 2")
+    plt.plot(dataset[key][4,:], 'ob')
+    plt.plot(dataset[key][5,:], 'o', color='orange')
     
-    #plt.plot(dataset[key][6,:], 'ob')
-    #plt.plot(dataset[key][7,:], 'o', color='orange')
+    #plt.figure("Torques "+  key + " sample 3")
+    plt.plot(dataset[key][6,:], 'ob')
+    plt.plot(dataset[key][7,:], 'o', color='orange')
     
+    #plt.figure("Torques "+  key + " sample 4")
     plt.plot(dataset[key][8,:], 'ob')
     plt.plot(dataset[key][9,:], 'o', color='orange')
+    
+    #plt.figure("Torques "+  key + " sample 5")
+    plt.plot(dataset[key][10,:], 'ob')
+    plt.plot(dataset[key][11,:], 'o', color='orange')
     
     plt.xlabel("Time")
     plt.ylabel("Torque")
     plt.legend(["Joint 1", "Joint 2"])
     plt.grid()
     scores = []
+    confusion = dict.fromkeys(['TP', 'FP', 'TN', 'FN'])
+    for entry in confusion.keys():
+        confusion[entry] = 0
     A_tilde, Phi, A = dmd.dmd(dataset[key][:2,:], 2)
     for sample in range(0,N_samples+1):
         N = dataset[key].shape[1]
@@ -49,38 +60,48 @@ for key in dataset.keys():
         prediction[:,0] = dataset[key][:2,0]
         score = 0
         for i in range(1,N):
-            tau_next = dataset[key][:2,i:i+1]
-            tau_predicted = dmd.dmd4cast(dataset[key][2*sample:2*sample+2,:i], 2, 1)
+            tau_next = dataset[key][2*sample:2*sample+2,i:i+1]
+            tau_predicted = dmd.dmd4cast(dataset[key][0:2,:i], 2, 1)
             prediction[:,i:i+1] = tau_predicted
-            a = np.greater((tau_next - tau_predicted), np.ones((2,1))*0.2)
-            b = np.less((tau_next - tau_predicted), -np.ones((2,1))*0.2)
-            if a.all() or b.all():
+            error = tau_next - tau_predicted
+            a = error >= np.ones((2,1))*0.2
+            #print('a',a)
+            b = error <= -np.ones((2,1))*0.2
+            #print('b',b)
+            if (a[0] or a[1]) or (b[0] or b[1]):
                 print(f"anomaly detected at sample {sample} and time {i}")
                 if disturbance_ground_truth[key][sample,i] == 1:
                     score += 1
+                    confusion['TP'] += 1
+                else:
+                    confusion['FP'] += 1
             else:
                 if disturbance_ground_truth[key][sample,i] == 0:
                     score += 1
-        print(f"Score for sample {sample} is {score/N}")
-        scores.append(score/N)
-        plt.figure(f"Predicted torques error {key} sample {sample}")
-        plt.plot(prediction[0,:]- dataset[key][0,:], 'b')
-        plt.plot(prediction[1,:]- dataset[key][1,:], 'orange')
-        plt.xlabel("Time")
-        plt.ylabel("Torque")
-        plt.legend(["Joint 1", "Joint 2"])
-        plt.grid()
-        #plt.show()
-    plt.figure("Scores")
-    plt.step(np.arange(0,N_samples+1), scores)
-    plt.xlabel("Sample")
-    plt.ylabel("Score")
+                    confusion['TN'] += 1
+                else:
+                    confusion['FN'] += 1
+                    
+        print(f"Score {score}")
+        print(f"Score for sample {sample} is {score/(N-1)}")
+        scores.append(score/(N-1))
+        
+    conf_matrix = np.array([[confusion['TP'], confusion['FP']], [confusion['FN'], confusion['TN']]])
+    fig, ax = plt.subplots(figsize=(7.5, 7.5))
+    ax.matshow(conf_matrix, cmap=plt.cm.Blues, alpha=0.3)
+    for i in range(conf_matrix.shape[0]):
+        for j in range(conf_matrix.shape[1]):
+            ax.text(x=j, y=i,s=conf_matrix[i, j], va='center', ha='center', size='xx-large')
+        
+    plt.xlabel('Predictions', fontsize=18)
+    plt.ylabel('Actuals', fontsize=18)
+    plt.title('Confusion Matrix', fontsize=18)
+    
+    scores = np.array(scores)
+    plt.figure("Prediction")
+    plt.bar(list(range(6)),scores)
+    plt.xlabel("sample")
+    plt.ylabel("Probability")    
     plt.grid()
+    
     plt.show()
-    
-    #pred_step = 2
-    #next_tau = dmd.DMD4cast(dataset[key], 1, pred_step)
-    
-    
-    
-plt.show()
--- src/dmd_perturbation_detection/dataset_builder.py
+++ src/dmd_perturbation_detection/dataset_builder.py
@@ -2,78 +2,54 @@ import dmd_perturbation_detection.trajectories as trajectories
 from dmd_perturbation_detection.robot import Robot
 import numpy as np
 import matplotlib.pyplot as plt
+import copy
 
-def create_dataset(rbt: Robot, start_points:np.ndarray, end_points:np.ndarray, execution_time:np.ndarray, N_samples:int, noise_level:float = 0.2)->dict:
+def create_dataset(rbt: Robot, start_points:np.ndarray, end_points:np.ndarray, execution_time:np.ndarray, N_samples:int, noise_level:float = 0.1)->dict:
     _, n = start_points.shape
     dataset = dict.fromkeys(["traj_" + str(key) for key in range(N_samples)])
     disturbance_ground_truth = dict.fromkeys(["traj_" + str(key) for key in range(N_samples)])
     for point in range(n):
+        # create the trajectory
         start_q = rbt.inverseKinimatics(start_points[0,point], start_points[1,point])
-        #print("Start point")
-        #print(start_points[:,point])
-        #print(start_q)
-        #print("Calculated point")
-        #print(rbt.forwardKinematics(start_q))
-        #rbt.plot_robot(start_q)
         end_q = rbt.inverseKinimatics(end_points[0,point], end_points[1,point])
-        #print("End point")
-        #print(end_points[:,point])
-        #print(end_q)
-        #print("Calculated point")
-        #print(rbt.forwardKinematics(end_q))
-        #rbt.plot_robot(end_q)
         traj = trajectories.createTrajectory(start_q, end_q, execution_time[point], 0.1)
-        #plt.figure(f"Joint position {point}")
-        #plt.plot(traj[0,:])
-        #plt.plot(traj[1,:])
-        #plt.grid()
-        #plt.legend(["Joint 1", "Joint 2"])
-        #plt.xlabel("Time")
-        #plt.ylabel("Position")
-        #plt.figure(f"Velocities {point}")
-        #plt.plot(traj[2,:])
-        #plt.plot(traj[3,:])
-        #plt.legend(["Joint 1", "Joint 2"])
-        #plt.xlabel("Time")
-        #plt.ylabel("Velocity")
-        #plt.grid()
-        #plt.figure(f"Accelerations {point}")
-        #plt.plot(traj[4,:])
-        #plt.plot(traj[5,:])
-        #plt.legend(["Joint 1", "Joint 2"])
-        #plt.xlabel("Time")
-        #plt.ylabel("Acceleration")
-        #plt.grid()
-        #plt.show()
+
+        # compute the required torque
         _, N = traj.shape
         required_torque = np.zeros((2,N))
         for i in range(N): 
-                    required_torque[:,i:i+1] = rbt.forward_dynamics(traj[:2,i:i+1], traj[2:4,i:i+1], traj[4:6,i:i+1])
-        #plt.figure("Torques")
-        #plt.plot(required_torque[0,:])
-        #plt.plot(required_torque[1,:])
-        #plt.grid()
-        #plt.show()
+            required_torque[:,i:i+1] = rbt.forward_dynamics(traj[:2,i:i+1], traj[2:4,i:i+1], traj[4:6,i:i+1])
+
+        # create the dataset
         data = np.zeros((2*(N_samples+1), N))
         dist_bool = np.zeros((N_samples+1, N))
         i = 0
         for sample in range(N_samples+1):
             if sample == 0:
-                data[i:i+2,:] = required_torque #insert the ground truth at the first row
+                data[i:i+2,:] = copy.deepcopy(required_torque) #insert the ground truth at the first row
+                print(data[i:i+2,:].shape)
             else:     
+                data[i:i+2,:] = required_torque + np.random.normal(0, noise_level, (2,N))
                 if sample == 2 :
-                    ext_force = np.array([[1.],[0.]]).reshape(2,1)
+                    ext_force = np.array([[2.],[0.]]).reshape(2,1)
+                elif sample == 3:
+                    ext_force = np.array([[1.],[1.]]).reshape(2,1)
                 elif sample == 4:
-                    ext_force = np.array([[0.],[1.]]).reshape(2,1)
+                    ext_force = np.array([[0.],[-2.]]).reshape(2,1)
                 else :
-                    ext_force = np.array([[0.],[0.]]).reshape(2,1)
-                disturbance_time = np.random.randint(0, N-10)
-                for j in range(disturbance_time, disturbance_time+10):
-                    required_torque[:,j:j+1] = rbt.forward_dynamics(traj[:2,j:j+1], traj[2:4,j:j+1], traj[4:6,j:j+1], ext_force)
-                    if ext_force.all() != 0:
+                    ext_force = np.array([[-1.],[1.]]).reshape(2,1)
+                
+                  
+                disturbance_time = np.random.randint(low=0, high=N-20)
+                #print(f"I'm here point, {point}, sample {sample}, disturbance time {disturbance_time}")  
+                for j in range(disturbance_time, disturbance_time+20):
+                    dist =rbt.forward_dynamics(traj[:2,j:j+1], traj[2:4,j:j+1], traj[4:6,j:j+1], ext_force)
+                    print(f"dist {dist.transpose()}")
+                    data[i:i+2,j:j+1] = dist
+                    if ext_force[0,0] != 0 or ext_force[1,0] != 0:
                         dist_bool[sample, j:j+1] = 1
 
-                data[i:i+2,:] = required_torque + np.random.normal(0, noise_level, (2,N))
+                
                 disturbance_ground_truth["traj_" + str(point)] = dist_bool
             dataset["traj_" + str(point)] = data
             i += 2
